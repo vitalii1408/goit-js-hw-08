97f41370602984a4a3b90506a9902b22be25bb6b
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-18 21:49:32 +0200

    6

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/plauyer';
+import { async } from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const plauyer = Player('vimeo-player');
+const STORAGE_KEY = 'videoplayer-current-time';
+const saveCurrentTime = throttle(async () => {
+  try {
+    const saveCurrentTime = await plauyer.getCurrentTime();
+  } catch (error) {
+    console.error('Error saving urent time:', error);
+  }
+}, 1000);
+
+const restoreCurrentTime = async () => {
+  if (savedTime) {
+    try {
+      const getCurrentTime = JSON.parse(savedTime);
+      await plauyer.setCurrentTime(currentTime);
+    } catch (error) {
+      console.error('Error restoring current time::', error);
+    }
+  }
+};
+plauyer.on('timeupdate', saveCurrentTime);
+document.addEventListener('DOMContentLoaded', () => {
+  restoreCurrentTime();
+});
