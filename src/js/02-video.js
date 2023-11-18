e3b6b0ba06f14b35769da7727c904156ffd70b37
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-18 22:54:14 +0200

    8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,19 @@
 import Player from '@vimeo/player';
 import { async } from '@vimeo/player';
 import throttle from 'lodash.throttle';
-
-const plauyer = Player('vimeo-player');
-const STORAGE_KEY = 'videoplayer-current-time';
-const saveCurrentTime = throttle(async () => {
-  try {
-    const saveCurrentTime = await plauyer.getCurrentTime();
-  } catch (error) {
-    console.error('Error saving urent time:', error);
-  }
-}, 1000);
-
-const restoreCurrentTime = async () => {
-  if (savedTime) {
-    try {
-      const getCurrentTime = JSON.parse(savedTime);
-      await plauyer.setCurrentTime(currentTime);
-    } catch (error) {
-      console.error('Error restoring current time::', error);
+document.addEventListener('DOMContentLoaded', function () {
+  const video = document.getElementById('vimeo-player');
+  const STORAGE_KEY = 'video-player-current-time';
+  const saveCurrentTime = () => {
+    localStorage.setItem(STORAGE_KEY, video.currentTime);
+  };
+  const restoreCurrentTime = () => {
+    const savedTime = localStorage.getItem(STORAGE_KEY);
+    if (savedTime !== null) {
+      video.currentTime = parseFloat(savedTime);
     }
-  }
-};
-plauyer.on('timeupdate', saveCurrentTime);
-document.addEventListener('DOMContentLoaded', () => {
+  };
+  video.addEventListener('timeupdate', saveCurrentTime);
+  window.addEventListener('beforeunload', saveCurrentTime);
   restoreCurrentTime();
 });
