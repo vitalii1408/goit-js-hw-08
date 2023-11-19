eb4be9d85c68d67b2bfaae9b5ff17421e469959b
vitalii1408 <135379297+vitalii1408@users.noreply.github.com>
2023-11-19 11:13:09 +0200

    10

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,21 @@
-import Player from '@vimeo/player';
+import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
-document.addEventListener('DOMContentLoaded', function () {
-  const player = new Vimeo(document.getElementById('vimeo-player'));
-  const savedTime = localStorage.getItem('videoplayer-current-time');
-  if (savedTime) {
-    player
-      .setCurrentTime(parseFloat(savedTime))
-      .then(function (seconds) {})
-      .catch(function (error) {
-        switch (error.name) {
-          case 'RangeError':
-            break;
 
-          default:
-            break;
-        }
-      });
+document.addEventListener('DOMContentLoaded', function () {
+  const iframe = document.getElementById('vimeo-player');
+  const player = new Vimeo(iframe);
+  const saveToLocalStorageThrottled = throttle(saveToLocalStorage, 1000);
+  player.on('timeupdate', saveToLocalStorageThrottled);
+  loadFromLocalStorage();
+  function saveToLocalStorage(event) {
+    const currentTime = event.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
   }
-  player.on(
-    'timeupdate',
-    throttle(function (data) {
-      localStorage.setItem('videoplayer-current-time', data.seconds);
-    }, 1000)
-  );
+  function loadFromLocalStorage() {
+    const storedTime = localStorage.getItem('videoplayer-current-time');
+    if (storedTime) {
+      const currentTime = parseFloat(storedTime);
+      player.setCurrentTime(currentTime);
+    }
+  }
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash/throttle';
 
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('[name="email"]');
+  const messageInput = form.querySelector('[name="message"]');
+  const feedbackFormStateKey = 'feedback-form-state';
+  const saveToLocalStorageThrottled = throttle(saveToLocalStorage, 500);
+  form.addEventListener('input', saveToLocalStorageThrottled);
+  loadFromLocalStorage();
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    clearLocalStorage();
+    console.log(getFormData());
+  });
+  function saveToLocalStorage() {
+    const formData = getFormData();
+    localStorage.setItem(feedbackFormStateKey, JSON.stringify(formData));
+  }
+
+  function loadFromLocalStorage() {
+    const storedData = localStorage.getItem(feedbackFormStateKey);
+    if (storedData) {
+      const formData = JSON.parse(storedData);
+      setFormData(formData);
+    }
+  }
+  function clearLocalStorage() {
+    localStorage.removeItem(feedbackFormStateKey);
+  }
+  function getFormData() {
+    return {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+  }
+  function setFormData(formData) {
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
+});
